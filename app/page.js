4f4be1df41dbb84cc9051ec617f3c3fced1aa955
carlos camacho <carlic.dev@gmail.com>
2024-12-05 18:36:56 -0600

    added services/graphql

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 
 import Featured from "./components/Featured";
 import Hero from "./components/Hero";
+import RecentPostsSection from "./components/RecentPostsSection";
 
 export default function Home() {
   return (
     <main className="w-full">
       <Hero />
       <Featured />
+      <RecentPostsSection />
     </main>
   );
 }
